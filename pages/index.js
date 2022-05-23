f2e128e8510f63cff9d76a89a0980b7289b453ab
Samuel Ordieres <samuordieres@gmail.com>
2022-05-23 22:08:21 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({eventos}) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by:{' '}
+          Powred by:{' '}
           <span className={styles.logo}>
             <Image src="https://onlyfest.es//images/logo-black-horizontal.png" alt="OnlyFest Logo" width={72} height={36} />
           </span>
