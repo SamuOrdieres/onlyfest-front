b7f4335fe519c2b96fd631cd779a921f08f7c8f3
Samuel Ordieres <samuordieres@gmail.com>
2022-05-26 19:32:26 +0200

    added responsive styles to cards
    Added responsive styles to cards

--- components/EventPreview.js
+++ components/EventPreview.js
@@ -5,17 +5,26 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Chip from '@mui/material/Chip';
+
+
 
 export default function EventPreview({evento}) {
+
+  function renderCategoryChips(categories){
+
+    return categories.map((category) => {
+        return <Chip key={category.id} label={category.attributes.Nombre} variant="outlined" />
+    })
+  }
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card>
       <CardActionArea>
       <Link href={`/eventos/${evento.id}`} >
       <CardMedia
         component="img"
-        height="140"
+        width="320"
         image={evento.attributes.ImagenPrincipal.data.attributes.formats.medium.url}
         alt="green iguana"
       />
@@ -30,9 +39,8 @@ export default function EventPreview({evento}) {
         </Typography>
       </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+      <CardActions sx={{ justifyContent: 'flex-end' }}>
+        {renderCategoryChips(evento.attributes.Categorias.data)}
       </CardActions>
     </Card>
   );
--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,18 @@ import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
 // Create a theme instance.
+const defaultTheme = createTheme({
+	breakpoints: {
+		values: {
+		  mobile: 0,
+		  tablet: 640,
+		  laptop: 1024,
+		  desktop: 1200,
+		},
+	  },
+	
+});
+
 const theme = createTheme({
 	palette: {
 		primary: {
@@ -13,15 +25,6 @@ const theme = createTheme({
 		error: {
 			main: red.A400,
 		},
-	},
-	components: {
-		MuiCard: {
-			styleOverrides:{
-				root:{
-					marginBottom: '2rem',
-				}
-			}
-		}
 	},
 	typography: {
 		fontFamily: [
@@ -37,6 +40,29 @@ const theme = createTheme({
 		  '"Segoe UI Symbol"',
 		].join(','),
 	  },
-});
+	components: {
+		MuiCard: {
+			styleOverrides:{
+				root:{
+					marginBottom: '2rem',
+					[defaultTheme.breakpoints.up('mobile')]: {
+						maxWidth: 300
+					},
+					[defaultTheme.breakpoints.up('tablet')]: {
+						maxWidth: 500					  
+					},
+					[defaultTheme.breakpoints.up('laptop')]: {
+						maxWidth: 800					
+					},
+					[defaultTheme.breakpoints.up('desktop')]: {
+						maxWidth: 1200					  
+					}
+				}
+			}
+		}
+	},
+})
+
+
 
 export default theme;
