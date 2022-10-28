005a01978bf4ebcd608c5271544d1b82150c170d
Samuel Ordieres <samuordieres@gmail.com>
2022-10-28 11:03:00 +0200

    Update LatestEvents.js

--- components/Events/LatestEvents.js
+++ components/Events/LatestEvents.js
@@ -1,24 +1,23 @@
-import React, {useEffect, useState} from 'react'
-import EventPreview from './EventPreview'
+import React, { useEffect, useState } from "react";
+import EventPreview from "./EventPreview";
 
-export default function LatestEvents({eventos}) {
-const [latestEvents, setLatestEvents] = useState([]);
+export default function LatestEvents({ eventos }) {
+  const [latestEvents, setLatestEvents] = useState([]);
 
-useEffect(() => {
-  setLatestEvents(eventos.slice(0, 3)); // Last 3 posts
-}, [eventos])
+  useEffect(() => {
+    setLatestEvents(eventos.slice(0, 5)); // Last 3 posts
+  }, [eventos]);
 
-function renderEventPreviews(){
-
+  function renderEventPreviews() {
     return latestEvents.map((evento) => {
-        return <EventPreview evento={evento} key={evento.id} />
-    })
-}
+      return <EventPreview evento={evento} key={evento.id} />;
+    });
+  }
 
   return (
-      <>    
-        <h2>Últimos eventos</h2>
-        {renderEventPreviews()}
-      </>
-  )
+    <>
+      <h2>Últimos eventos</h2>
+      {renderEventPreviews()}
+    </>
+  );
 }
